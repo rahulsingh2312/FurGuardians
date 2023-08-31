d4552e1c685750f7511be17d681ce23016f223c0
Rahul <rahulsinghhh2312@gmail.com>
2023-08-31 19:20:48 +0530

    before nav done

---
+++ front-furguardians/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Map from './map';
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+    <Map />
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ front-furguardians/src/map.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+}
+
+function MapComponent() {
+  const position = [40.505, -0.09];
+
+  return (
+    <div className="map-container">
+      <MapContainer center={position} zoom={13} style={{ width: '100%', height: '100vh' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            furGuardian #69
+          </Popup>
+        </Marker>
+
+        {/* LocationMarker Component */}
+        <LocationMarker />
+        {/* LocationMarker Component End */}
+      </MapContainer>
+    </div>
+  );
+}
+
+export default MapComponent;
